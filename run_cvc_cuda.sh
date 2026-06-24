12b8e9a8a4ba97a66f4adfd26b9dea7dedfad92f
Lynx solutions <jakubpelka@gmail.com>
2026-06-24 21:28:49 +0200

    Update run_cvc_cuda.sh

--- run_cvc_cuda.sh
+++ run_cvc_cuda.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-cd "$HOME/GitHub/ComputerVisionCounter_video"
+
+cd "$HOME/GitHub/ComputerVisionCounter_video" || exit 1
+
 source .venv_cuda/bin/activate
-python src/start.py
+
+# Wymusza RTSP over TCP dla OpenCV/FFmpeg.
+# To ogranicza błędy H.264 typu:
+# "error while decoding MB ..., bytestream ..."
+export OPENCV_FFMPEG_CAPTURE_OPTIONS="rtsp_transport;tcp"
+
+python src/start.py "$@"
